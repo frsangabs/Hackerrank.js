5776f3f933aac7c3a60cc4dbac16aef83fdbf6bf
frsangabs <168558850+frsangabs@users.noreply.github.com>
2024-08-19 14:32:30 -0300

    Concluded 03_compareTheTriplets.js
    ex concluído

--- 03_compareTheTriplets.js
+++ 03_compareTheTriplets.js
@@ -32,6 +32,17 @@ function readLine() {
  */
 
 function compareTriplets(a, b) {
+    let alice = 0
+    let bob = 0
+     for(let i = 0; i < a.length; i++){
+         if(a[i]>b[i]){
+             alice++
+         }else if(b[i]>a[i]){
+             bob ++
+         }
+     }
+     let lista = [alice, bob]
+     return lista
 }
 
 function main() {
