5c085c4b7709f04dd6fa519bc4847c1303fe86df
frsangabs <168558850+frsangabs@users.noreply.github.com>
2024-08-19 14:30:59 -0300

    Concluded 02_simpleArraySum.js
    ex concluído

--- 02_simpleArraySum.js
+++ 02_simpleArraySum.js
@@ -31,6 +31,11 @@ function readLine() {
 
 function simpleArraySum(ar) {
     // Write your code here
+    let soma = 0
+    for(let i = 0; i < ar.length; i++){
+        soma += ar[i]
+    }
+    return soma
 }
 
 function main() {
