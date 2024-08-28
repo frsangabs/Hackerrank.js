08a66cabdaa667bc46a583cc060c53e135aa162a
frsangabs <168558850+frsangabs@users.noreply.github.com>
2024-08-27 22:50:53 -0300

    Concluded 10_plusMinus.js
    ex concluído

--- 10_plusMinus.js
+++ 10_plusMinus.js
@@ -28,6 +28,25 @@ function readLine() {
 
 function plusMinus(arr) {
     // Write your code here
+    
+    let positivo = 0
+    let zero = 0
+    let negativo = 0
+    
+    for(let i = 0; i<arr.length; i++){
+        if(arr[i]>0){
+            positivo++
+        }
+        if(arr[i]==0){
+            zero++
+        }
+        if(arr[i]<0){
+            negativo++
+        }
+    }
+    console.log((positivo/arr.length).toFixed(6))
+    console.log((negativo/arr.length).toFixed(6))
+    console.log((zero/arr.length).toFixed(6))
 
 }
 
