7fe395c1efc076ba00fbd38df9eeeb02eede1cf0
frsangabs <168558850+frsangabs@users.noreply.github.com>
2024-09-02 08:36:06 -0300

    Concluded 12_timeConversion.js
    ex concluído

--- 12_timeConversion.js
+++ 12_timeConversion.js
@@ -31,6 +31,26 @@ function readLine() {
 
 function timeConversion(s) {
     // Write your code here
+    
+    s.split('')
+    
+    let hours = parseInt(s[0] + s[1])
+    let minutes = s[3] + s[4]
+    let seconds = s[6] + s[7]
+    let ampm = s[s.length-2]
+    
+    if(hours >= 12 && ampm === 'A'){
+        hours = hours -12
+        }else if(hours < 12 && ampm === 'P'){
+            hours = hours +12
+    }
+    
+    
+    if(hours < 10){
+        return (`0${hours}:${minutes}:${seconds}`)
+    }else {
+        return (`${hours}:${minutes}:${seconds}`)
+    }
 }
 
 function main() {
